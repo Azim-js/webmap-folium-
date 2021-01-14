fe5f494605804a0abcb4d3fdcdc7be85a39d7528
Azim <66790056+Azim-js@users.noreply.github.com>
2021-01-14 22:07:40 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,10 @@
 import folium
 
+# folium creats leaflet aotumatcalliy in the html 
+# now using the folium object called .Map()
+
 x=folium.Map(location=[13.015778943432096, 77.60272672869591],zoom_start=50)
+
+#and outputing the map in map2.html 
+
 x.save("map2.html")
